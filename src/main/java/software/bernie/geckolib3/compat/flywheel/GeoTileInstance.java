846a8094a37e13516112adcebd98f09423238ead
Jozufozu <jozsefaug@gmail.com>
2022-02-01 12:03:41 -0800

    Fix instancing creating too many top level bones
    - Bump flywheel version
    - Fix throws formatting in editorconfig

--- src/main/java/software/bernie/geckolib3/compat/flywheel/GeoTileInstance.java
+++ src/main/java/software/bernie/geckolib3/compat/flywheel/GeoTileInstance.java
@@ -44,7 +44,7 @@ public class GeoTileInstance<T extends BlockEntity & IAnimated> extends BlockEnt
 
 		stack.translate(getInstancePosition()).translate(0.5, 0.01, 0.5).rotateToFace(getFacing());
 
-		for (AnimatingBone bone : model.getAllBones()) {
+		for (AnimatingBone bone : model.getTopLevelBones()) {
 			topLevelBones.add(new GeoInstanceTree(materialManager, state, bone));
 		}
 
