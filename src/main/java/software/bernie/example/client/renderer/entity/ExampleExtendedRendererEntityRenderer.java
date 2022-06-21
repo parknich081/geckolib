4ad5a5eac1d57ec6d4ec4acdec9b72efb704ef5a
AzureZhen <rcraft@cyberconnects2.com>
2022-06-21 19:31:52 -0400

    v3.1.2

--- src/main/java/software/bernie/example/client/renderer/entity/ExampleExtendedRendererEntityRenderer.java
+++ src/main/java/software/bernie/example/client/renderer/entity/ExampleExtendedRendererEntityRenderer.java
@@ -14,8 +14,8 @@ import net.minecraft.util.math.Vec3f;
 import software.bernie.example.client.DefaultBipedBoneIdents;
 import software.bernie.example.client.model.entity.ExampleExtendedRendererEntityModel;
 import software.bernie.example.entity.ExtendedRendererEntity;
-import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.GeckoLib;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.renderers.geo.ExtendedGeoEntityRenderer;
 
@@ -171,12 +171,14 @@ public class ExampleExtendedRendererEntityRenderer extends ExtendedGeoEntityRend
 	}
 
 	@Override
-	protected void preRenderBlock(BlockState block, String boneName, ExtendedRendererEntity currentEntity) {
+	protected void preRenderBlock(MatrixStack stack, BlockState block, String boneName,
+			ExtendedRendererEntity currentEntity) {
 
 	}
 
 	@Override
-	protected void postRenderBlock(BlockState block, String boneName, ExtendedRendererEntity currentEntity) {
+	protected void postRenderBlock(MatrixStack stack, BlockState block, String boneName,
+			ExtendedRendererEntity currentEntity) {
 	}
 
 	protected final Identifier CAPE_TEXTURE = new Identifier(GeckoLib.ModID,
